23492684d4af9daa4f6c05583928db67b48caeff
Sergey Nartimov <sergey@bookfusion.com>
2024-02-26 14:31:55 +0100

    Bump version

--- version-bump.mjs
+++ version-bump.mjs
@@ -6,9 +6,9 @@ const targetVersion = process.env.npm_package_version
 const manifest = JSON.parse(readFileSync('manifest.json', 'utf8'))
 const { minAppVersion } = manifest
 manifest.version = targetVersion
-writeFileSync('manifest.json', JSON.stringify(manifest, null, '\t'))
+writeFileSync('manifest.json', JSON.stringify(manifest, null, '  ') + '\n')
 
 // update versions.json with target version and minAppVersion from manifest.json
 const versions = JSON.parse(readFileSync('versions.json', 'utf8'))
 versions[targetVersion] = minAppVersion
-writeFileSync('versions.json', JSON.stringify(versions, null, '\t'))
+writeFileSync('versions.json', JSON.stringify(versions, null, '  ') + '\n')
